bb35ac9232f726d01b4ae585032c1fbc8b4c0981
simonryrie <simonryrie@gmail.com>
2023-05-30 12:53:33 +0100

    Challenges complete

--- challenge/read-file.js
+++ challenge/read-file.js
@@ -1,7 +1,15 @@
 const fs = require("node:fs");
+const { readFile } = require('node:fs');
 
 function readFilePromise(filePath) {
-  //
+  const promise = new Promise ((resolve, reject) => {
+  readFile(filePath, "utf8", (err, data) => {
+    if(err) {
+      reject(err);
+    };
+    resolve(data);
+  })})
+  return promise
 }
 
 module.exports = { readFilePromise };
--- challenge/wait.js
+++ challenge/wait.js
@@ -1,5 +1,7 @@
 function wait(ms) {
-  //
-}
+  const promise = new Promise ((resolve, reject) => {
+    setTimeout(() => {resolve("success")}, ms)});
+    return promise
+  }
 
 module.exports = { wait };
